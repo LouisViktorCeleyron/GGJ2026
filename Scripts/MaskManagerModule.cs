b8d50546d24964a58df39cba1f8d21bc8e7d8b73
Louis Celeyron <louis.celeyron@gmail.com>
2026-02-01 02:32:20 +0100

    Fin de journée

--- Scripts/MaskManagerModule.cs
+++ Scripts/MaskManagerModule.cs
@@ -9,22 +9,36 @@ public partial class MaskManagerModule : Module
 	[Export]
 	private Node2D _topPos,_botPos;
 
+	private Sprite2D _topSprite,_botSprite;
 
+	private Vector2 _baseTopPos, _baseBotPos;
 	public override void PostReady()
 	{
 		base.PostReady();
+		_baseTopPos = _topPos.Position;
+		_baseBotPos = _botPos.Position;
 		//GenerateMasks();
 	}
 
+	public void MaskFlip(bool flip)
+	{
+		
+		 _topPos.Position =new Vector2(_baseTopPos.X*(flip?-1:1),_topPos.Position.Y);
+		 _topSprite.FlipH = flip;
+		 _botPos.Position = new Vector2(_baseBotPos.X*(flip?-1:1),_botPos.Position.Y);
+		 _botSprite.FlipH = flip;
+	}
 	public void GenerateMasks()
 	{
 		var topMask = _topMasks.PickRandom();
 		var spawnedTop = topMask.Instantiate();
+		_topSprite = spawnedTop.GetChild<Sprite2D>(0);
 		_topPos.AddChild(spawnedTop);
 		//CallDeferred("AddChildToOwner",spawnedTop,_topPos);
 
 		var botMask = _botMasks.PickRandom();
 		var spawnedBot = botMask.Instantiate();
+		_botSprite= spawnedBot.GetChild<Sprite2D>(0);
 		_botPos.AddChild(spawnedBot);
 		//CallDeferred("AddChildToOwner",spawnedBot,_botPos);
 		
